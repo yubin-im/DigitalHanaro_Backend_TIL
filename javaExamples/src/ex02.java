12a5f07ed95f3a9183a31338a08cc446d5fb4f1f
yubin-im <ybyb130@gmail.com>
2024-03-07 13:35:13 +0900

    02 주석, 출력문 실습

---
+++ javaExamples/src/ex02.java
@@ -0,0 +1,26 @@
+public class ex02 {
+    public static void main(String[] args) {
+        // 한 줄 주석문
+        /*
+        여러줄
+        주석문
+         */
+
+        // 출력문
+        System.out.println("화면 출력");
+        System.out.println("화면" + "출력");
+        System.out.println("화면" + 10 + 20);
+        System.out.println(10 + 20 + "화면");
+
+        // println, print, printf
+        // println: 문자열 한 줄 출력 + 줄바꿈
+        // print: 줄바꿈 없음
+        // printf: 형식화된 출력문
+        System.out.println("한 줄 출력후 줄바꿈");
+        System.out.print("한 줄 출력후 줄바꿈 없음");
+        System.out.printf("%d\n", 30); // 10진수
+        System.out.printf("%o\n", 30); // 8진수
+        System.out.printf("%x\n", 30); // 16진수
+        System.out.printf("%e\n", 300.0); // 지수형 출력
+    }
+}
