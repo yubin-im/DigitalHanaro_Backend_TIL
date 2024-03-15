0e4bd3dd2ea0278844735f868e617ec5740670c2
yubin-im <ybyb130@gmail.com>
2024-03-15 09:06:15 +0900

    08 서버, 클라이언트 소켓 실습

---
+++ javaExamples/src/unit08_Thread_IO/ex62_server.java
@@ -0,0 +1,61 @@
+package unit08_Thread_IO;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+// 테스트 방법
+// 1. 서버 소켓을 먼저 실행함.
+// 2. 클라이언트 소켓을 나중에 실행함.
+
+public class ex62_server {
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+
+        try {
+
+            serverSocket = new ServerSocket();
+            serverSocket.bind(
+                    new InetSocketAddress("172.16.20.245", 5009));
+            while (true) {
+                System.out.println("서버가 연결을 기다리는 중...");
+                Socket socket = serverSocket.accept(); // Blocking..대기중..
+                InetSocketAddress isa =
+                        (InetSocketAddress) socket.getRemoteSocketAddress();
+                System.out.println("서버가 연결을 수락함:" + isa.getHostName());
+
+                // 클라이언트 데이터를 수신한다.
+                byte[] bytes = null;
+                String message = null;
+
+                InputStream is = socket.getInputStream();
+                bytes = new byte[1024]; //1024바이트만큼 버퍼메모리 생성
+                int readByteCount = is.read(bytes);
+                message = new String(bytes, 0, readByteCount, "UTF-8");
+                System.out.println("서버가 데이터 받기 성공함.");
+                System.out.println("서버가 받은 데이터:" + message);
+
+                // 서버에서 클라이언트로 데이터 보내기
+                OutputStream os = socket.getOutputStream();
+                message = "Hi~ This is Server~";
+                bytes = message.getBytes("UTF-8");
+                os.write(bytes);
+                os.flush(); // 버퍼에 있는 데이터 밀어내기
+                System.out.println("서버가 클라이언트에게 데이터 보내기 성공");
+
+                is.close();
+                os.close();
+                socket.close();
+                if (message.equals("exit")) { // "exit"문자열이면 서버 종료!
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("서버 초기화 에러!");
+        }
+
+    }
+}
+
