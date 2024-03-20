9133caa4bc2d0b3f475970c84f888749fa59aacc
yubin-im <ybyb130@gmail.com>
2024-03-20 13:07:31 +0900

    SQL: 03 단일행 함수 실습

--- sql/ch03_function.sql
+++ sql/ch03_function.sql
@@ -85,3 +85,63 @@ SELECT RAND();
 SELECT RAND(100);
 SELECT ROUND(RAND() * 100); -- 0~100
 SELECT ROUND(RAND() * 100) + 1; -- 1~100
+
+-- 현재 날짜시간 가져오기
+SELECT NOW(), SYSDATE();
+
+-- 현재 날짜 가져오기
+SELECT CURDATE();
+
+-- 현재 시간 가져오기
+SELECT CURTIME(); 
+
+-- 날짜 간격 구하기
+SELECT NOW(),
+DATEDIFF('2025-12-20', NOW()),
+DATEDIFF(NOW(), '2025-12-20');
+
+SELECT NOW(),
+TIMESTAMPDIFF(YEAR, NOW(), '2025-12-20'),
+TIMESTAMPDIFF(MONTH, NOW(), '2025-12-20'),
+TIMESTAMPDIFF(DAY, NOW(), '2025-12-20');
+
+SELECT NOW(),
+DATEDIFF('2024-3-21 9:34', NOW()),
+TIMESTAMPDIFF(DAY, NOW(), '2024-3-21 9:34');
+
+SELECT NOW()
+		,ADDDATE(NOW(), 50) -- 50일후
+		,ADDDATE(NOW(), INTERVAL 50 DAY) -- 50일후
+		,ADDDATE(NOW(), INTERVAL 50 MONTH) -- 50달후
+		,SUBDATE(NOW(), INTERVAL 50 HOUR); -- 50시간전
+
+SELECT NOW()
+		,LAST_DAY(NOW())
+		,DAYOFYEAR(NOW()) 
+		,MONTHNAME(NOW()) 
+		,WEEKDAY(NOW());  -- 월(0)~일(6)
+		
+ -- 형변환 함수
+SELECT CAST('1' AS UNSIGNED); -- 부호없는 숫자로 변환
+SELECT CAST(2 AS CHAR(1)); -- char형으로 변환
+SELECT CONVERT('1', UNSIGNED);    
+SELECT CONVERT(2, CHAR(1));
+
+-- 조건 함수 (자바의 삼항연산자와 유사)
+SELECT IF( 10 > 20, '10', '20');
+SELECT IF( 12500 * 450 > 500000, '초과달성', '미달성');
+
+-- null 체크 함수
+SELECT IFNULL(1, 0);
+SELECT IFNULL(null, 0);
+SELECT IFNULL(null, 'null입니다.');
+
+SELECT NULLIF( 12 * 10, 120 ); 
+SELECT NULLIF( 12 * 10, 1200 ); 
+
+-- 조건문 SELECT CASE문
+SELECT CASE 
+	WHEN 20 < 20 THEN '20보다 작음'
+	WHEN 20 < 30 THEN '30보다 작음'
+	ELSE '그외의 수'
+END;
