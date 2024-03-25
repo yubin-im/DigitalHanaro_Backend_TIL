fb6d330ddc2c907d74ccb2f3cd630eecf5670139
yubin-im <ybyb130@gmail.com>
2024-03-25 14:03:22 +0900

    Spring: 03 개발 환경 세팅 및 프로젝트 생성

---
+++ springboot/Ex03Autowired/src/test/java/com/study/ex03autowired/Ex03AutowiredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.ex03autowired;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex03AutowiredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
