f25516dfb8833fe26039b7c7e4a89454df492e89
yubin-im <ybyb130@gmail.com>
2024-03-25 11:29:41 +0900

    Spring: 02 개발 환경 세팅 및 프로젝트 생성

---
+++ springboot/Ex02SpringDI/src/main/java/com/study/ex02springdi/Ex02SpringDiApplication.java
@@ -0,0 +1,13 @@
+package com.study.ex02springdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex02SpringDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex02SpringDiApplication.class, args);
+	}
+
+}
---
+++ springboot/Ex02SpringDI/src/test/java/com/study/ex02springdi/Ex02SpringDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.ex02springdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex02SpringDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
