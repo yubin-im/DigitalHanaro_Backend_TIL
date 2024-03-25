7548c181d69ac60f223268a2f312849e47d5e315
yubin-im <ybyb130@gmail.com>
2024-03-25 09:37:25 +0900

    Spring: 01 개발 환경 세팅 및 프로젝트 생성

---
+++ springboot/Ex01FirstApp/src/main/java/com/study/Ex01FirstApp/Ex01FirstAppApplication.java
@@ -0,0 +1,13 @@
+package com.study.Ex01FirstApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex01FirstAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex01FirstAppApplication.class, args);
+	}
+
+}
