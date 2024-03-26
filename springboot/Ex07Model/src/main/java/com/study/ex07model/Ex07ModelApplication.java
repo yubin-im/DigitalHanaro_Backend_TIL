761dc72d7da523f23fb15ed61f036564061c828a
yubin-im <ybyb130@gmail.com>
2024-03-26 15:10:27 +0900

    Spring: ex07 개발환경 세팅 및 프로젝트 생성

---
+++ springboot/Ex07Model/src/main/java/com/study/ex07model/Ex07ModelApplication.java
@@ -0,0 +1,13 @@
+package com.study.ex07model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex07ModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex07ModelApplication.class, args);
+    }
+
+}
---
+++ springboot/Ex07Model/src/test/java/com/study/ex07model/Ex07ModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.ex07model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex07ModelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
