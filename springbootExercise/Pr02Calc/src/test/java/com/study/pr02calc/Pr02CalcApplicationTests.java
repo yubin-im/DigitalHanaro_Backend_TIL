62dc2db8bc96777def6742ee223e5f9df6b30898
yubin-im <ybyb130@gmail.com>
2024-03-26 11:14:16 +0900

    스프링 연습문제: 02 계산기 프로젝트 생성

---
+++ springbootExercise/Pr02Calc/src/test/java/com/study/pr02calc/Pr02CalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.pr02calc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pr02CalcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
