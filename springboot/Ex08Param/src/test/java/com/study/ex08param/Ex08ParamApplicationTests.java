6e21de07b5a02bf81a582cdb42e865a7c6bab0cd
yubin-im <ybyb130@gmail.com>
2024-03-26 15:35:46 +0900

    Spring: ex08 개발환경 세팅 및 프로젝트 생성

---
+++ springboot/Ex08Param/src/test/java/com/study/ex08param/Ex08ParamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.ex08param;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex08ParamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
