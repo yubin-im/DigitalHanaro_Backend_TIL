b21f167325291dafb6dd5f822baffe2bf09f20fe
yubin-im <ybyb130@gmail.com>
2024-03-26 10:14:20 +0900

    스프링 연습문제: 01 카운터 프로젝트 생성

---
+++ springbootExercise/Pr01Counter/src/main/java/com/example/pr01counter/Pr01CounterApplication.java
@@ -0,0 +1,13 @@
+package com.example.pr01counter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pr01CounterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pr01CounterApplication.class, args);
+    }
+
+}
