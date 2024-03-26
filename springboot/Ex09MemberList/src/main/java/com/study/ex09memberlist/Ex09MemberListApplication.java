f1ff0297a7a29297aacf4a358ab70e7607d838ea
yubin-im <ybyb130@gmail.com>
2024-03-26 16:16:47 +0900

    ex09 개발환경 세팅 및 프로젝트 생성

---
+++ springboot/Ex09MemberList/src/main/java/com/study/ex09memberlist/Ex09MemberListApplication.java
@@ -0,0 +1,13 @@
+package com.study.ex09memberlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex09MemberListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex09MemberListApplication.class, args);
+    }
+
+}
---
+++ springboot/Ex09MemberList/src/test/java/com/study/ex09memberlist/Ex09MemberListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.ex09memberlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex09MemberListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
