c7013a3a57a538ccec86c67135af541b92667be5
yubin-im <ybyb130@gmail.com>
2024-03-27 17:30:18 +0900

    스프링 연습문제: 04 카운터API- 프로젝트 생성

---
+++ springbootExercise/Pr04CounterAPI/src/test/java/com/study/pr04counterapi/Pr04CounterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.pr04counterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pr04CounterApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
