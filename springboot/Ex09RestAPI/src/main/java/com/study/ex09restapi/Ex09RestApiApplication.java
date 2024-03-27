24c4dfa7be94f53d6f2dbfc3688e995cdc76fa44
yubin-im <ybyb130@gmail.com>
2024-03-27 13:00:37 +0900

    Spring: ex09 RestAPI 프로젝트 생성

---
+++ springboot/Ex09RestAPI/src/main/java/com/study/ex09restapi/Ex09RestApiApplication.java
@@ -0,0 +1,13 @@
+package com.study.ex09restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex09RestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex09RestApiApplication.class, args);
+    }
+
+}
---
+++ springboot/Ex09RestAPI/src/test/java/com/study/ex09restapi/Ex09RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.ex09restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex09RestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
