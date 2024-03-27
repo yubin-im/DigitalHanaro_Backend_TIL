791ef59d1f1c9bcd0c0ce32fd92234cd8acdadf5
yubin-im <ybyb130@gmail.com>
2024-03-27 09:26:32 +0900

    스프링 연습문제: 03 자판기- 프로젝트 생성

---
+++ springbootExercise/Pr03VM/src/test/java/com/study/pr03vm/Pr03VmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.pr03vm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pr03VmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
