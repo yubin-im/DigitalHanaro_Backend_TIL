6392dd01972ce33b3c20e72aadd100c8412300c9
yubin-im <ybyb130@gmail.com>
2024-03-28 10:23:05 +0900

    Spring: ex10 Devtools 프로젝트생성

---
+++ springboot/Ex10Devtools/src/main/java/com/study/ex10devtools/Ex10DevtoolsApplication.java
@@ -0,0 +1,13 @@
+package com.study.ex10devtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex10DevtoolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex10DevtoolsApplication.class, args);
+    }
+
+}
---
+++ springboot/Ex10Devtools/src/test/java/com/study/ex10devtools/Ex10DevtoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.ex10devtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex10DevtoolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
