cad8d97ef801d1da7909db796928c5e661412458
yubin-im <ybyb130@gmail.com>
2024-03-28 15:36:42 +0900

    스프링 연습문제: 05 계산기API- 프로젝트 생성

---
+++ springbootExercise/Pr05CalcAPI/src/main/java/com/study/pr05calcapi/Pr05CalcApiApplication.java
@@ -0,0 +1,13 @@
+package com.study.pr05calcapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pr05CalcApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pr05CalcApiApplication.class, args);
+    }
+
+}
