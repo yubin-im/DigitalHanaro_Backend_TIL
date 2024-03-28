61a3235ba8996ed9847c2341f13d44b4aba85c7c
yubin-im <ybyb130@gmail.com>
2024-03-28 11:01:23 +0900

    Spring: ex11 MessageInter 프로젝트 생성

---
+++ springboot/Ex11MessageInter/src/main/java/com/study/ex11messageinter/Ex11MessageInterApplication.java
@@ -0,0 +1,13 @@
+package com.study.ex11messageinter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex11MessageInterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex11MessageInterApplication.class, args);
+    }
+
+}
