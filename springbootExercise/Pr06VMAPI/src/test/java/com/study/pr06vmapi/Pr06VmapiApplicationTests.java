decae96e0f43d4683cbcb570d398273c8dd271c7
yubin-im <ybyb130@gmail.com>
2024-03-29 09:13:02 +0900

    스프링 연습문제: 06 자판기API- 프로젝트 생성

---
+++ springbootExercise/Pr06VMAPI/src/test/java/com/study/pr06vmapi/Pr06VmapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.pr06vmapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pr06VmapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
