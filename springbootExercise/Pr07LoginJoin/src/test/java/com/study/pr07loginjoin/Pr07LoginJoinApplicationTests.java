f96b92254bac9f84c72fb4d6c355710fa8dc8a5a
yubin-im <ybyb130@gmail.com>
2024-03-29 15:52:22 +0900

    스프링 연습문제: 07 로그인/회원가입- 프로젝트 생성

---
+++ springbootExercise/Pr07LoginJoin/src/test/java/com/study/pr07loginjoin/Pr07LoginJoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.pr07loginjoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pr07LoginJoinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
