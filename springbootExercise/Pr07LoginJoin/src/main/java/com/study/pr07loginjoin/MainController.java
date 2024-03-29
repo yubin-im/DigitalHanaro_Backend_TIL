548de0b1992fc031176a93e7d201395d3beac88e
yubin-im <ybyb130@gmail.com>
2024-03-29 17:38:38 +0900

    스프링 연습문제: 07 로그인/회원가입- 기능구현 완료 (아이디 입력/중복 확인, 약관동의 확인)

--- springbootExercise/Pr07LoginJoin/src/main/java/com/study/pr07loginjoin/MainController.java
+++ springbootExercise/Pr07LoginJoin/src/main/java/com/study/pr07loginjoin/MainController.java
@@ -1,26 +1,27 @@
 package com.study.pr07loginjoin;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
-public class ViewController {
+public class MainController {
     private Member member;
     private List<Member> memberList = new ArrayList<>();
 
+    // 메인 출력
     @GetMapping("/")
     public String main() {
         return "login";
     }
 
+    // 로그인
     @PostMapping("/")
     public String login(@RequestParam("inputName") String inputName,
                         @RequestParam("inputPw") String inputPw,
@@ -38,11 +39,13 @@ public class ViewController {
         return "login";
     }
 
+    // 회원가입 폼 출력
     @GetMapping("/join")
     public String viewJoin() {
         return "join";
     }
 
+    // 회원가입
     @PostMapping("/join")
     public String join(@RequestParam("inputName") String inputName,
                        @RequestParam("inputEmail") String inputEmail,
@@ -58,5 +61,23 @@ public class ViewController {
         model.addAttribute("memberList", memberList);
         System.out.println("가입한 회원 정보: " + memberList);
         return "redirect:/";
+    }
+
+    @PostMapping("/check_duplicate")
+    @ResponseBody
+    public Map<String, Boolean> checkDuplicate(@RequestBody Map<String, String> requestBody) {
+        String inputName = requestBody.get("inputName");
+        boolean duplicate = false;
+
+        for(Member member: memberList) {
+            if(member.getUsername().equals(inputName)) {
+                duplicate = true;
+                break;
+            }
+        }
+
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("duplicate", duplicate);
+        return response;
     }
 }
