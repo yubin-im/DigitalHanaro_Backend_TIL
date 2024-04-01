7160974acdbd1d7bc8af8139d3952ce0a38b7a3f
yubin-im <ybyb130@gmail.com>
2024-04-01 13:21:02 +0900

    Spring: ex12 H2 DB 프로젝트 생성

---
+++ springboot/Ex12H2DB/src/main/java/com/study/ex12h2db/Ex12H2DbApplication.java
@@ -0,0 +1,13 @@
+package com.study.ex12h2db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex12H2DbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex12H2DbApplication.class, args);
+    }
+
+}
---
+++ springboot/Ex12H2DB/src/test/java/com/study/ex12h2db/Ex12H2DbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.ex12h2db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex12H2DbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
