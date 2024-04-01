a8b70b6d4548fcfb9c53b2aefc67605e5a380abd
yubin-im <ybyb130@gmail.com>
2024-04-01 16:17:43 +0900

    스프링 연습문제: 08 로그인/회원가입 API- 프로젝트 생성

---
+++ javaExercise/Pr08LoginJoinAPI/src/main/java/com/study/pr08loginjoinapi/Pr08LoginJoinApiApplication.java
@@ -0,0 +1,13 @@
+package com.study.pr08loginjoinapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pr08LoginJoinApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pr08LoginJoinApiApplication.class, args);
+    }
+
+}
---
+++ javaExercise/Pr08LoginJoinAPI/src/test/java/com/study/pr08loginjoinapi/Pr08LoginJoinApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.pr08loginjoinapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pr08LoginJoinApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
