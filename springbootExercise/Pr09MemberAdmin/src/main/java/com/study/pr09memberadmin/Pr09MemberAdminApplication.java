4c0983d55bd7594f7c5f2f1e8b36c700aa86fdeb
yubin-im <ybyb130@gmail.com>
2024-04-04 17:07:33 +0900

    스프링 연습문제: 09 회원 관리자 페이지 프로젝트 생성

---
+++ springbootExercise/Pr09MemberAdmin/src/main/java/com/study/pr09memberadmin/Pr09MemberAdminApplication.java
@@ -0,0 +1,13 @@
+package com.study.pr09memberadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pr09MemberAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pr09MemberAdminApplication.class, args);
+    }
+
+}
---
+++ springbootExercise/Pr09MemberAdmin/src/test/java/com/study/pr09memberadmin/Pr09MemberAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.pr09memberadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pr09MemberAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
