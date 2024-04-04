cbcfbad6caaa80dd2fd7d6c575f077d279b0406b
yubin-im <ybyb130@gmail.com>
2024-04-04 10:54:46 +0900

    Spring: ex14 MySQL DB 연동 실습

---
+++ springboot/Ex14ReadDB/src/main/java/com/study/ex14readdb/MainController.java
@@ -0,0 +1,23 @@
+package com.study.ex14readdb;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class MainController {
+    // 생성자 주입
+    final MemberRepository memberRepository;
+
+    @GetMapping("/")
+    public String main(Model model) {
+        List<MemberEntity> list = memberRepository.findAll();
+        model.addAttribute("list", list);
+
+        return "index";
+    }
+}
---
+++ springboot/Ex14ReadDB/src/main/java/com/study/ex14readdb/MemberRepository.java
@@ -0,0 +1,16 @@
+package com.study.ex14readdb;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+    // 기본 메소드
+    // 1. findAll: SELECT *
+    // 2. findById: SELECT * WHERE id = :id
+    // 3. count(): SELECT COUNT(*)
+    // 4. save(): UPDATE, INSERT
+    // 5. delete() : DELETE
+
+    // 쿼리 메소드, JPQL, NativeSQL 아래에 등록한다.
+}
