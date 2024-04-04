ab06023435672fb6e3afb04f8ff7b8725cbf1940
yubin-im <ybyb130@gmail.com>
2024-04-04 09:20:28 +0900

    Spring: ex14 MySQL DB 연동 프로젝트 생성

---
+++ springboot/Ex14ReadDB/src/test/java/com/study/ex14readdb/Ex14ReadDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.ex14readdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex14ReadDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
