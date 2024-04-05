4c475907f92eba17b74d78d934846d710c860540
yubin-im <ybyb130@gmail.com>
2024-04-05 14:02:35 +0900

    스프링 연습문제: 10 회원 관리자 페이지 API- 프로젝트 생성

---
+++ springbootExercise/Pr10MemberAdminAPI/src/main/java/com/study/pr10memberadminapi/Pr10MemberAdminApiApplication.java
@@ -0,0 +1,13 @@
+package com.study.pr10memberadminapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pr10MemberAdminApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pr10MemberAdminApiApplication.class, args);
+    }
+
+}
