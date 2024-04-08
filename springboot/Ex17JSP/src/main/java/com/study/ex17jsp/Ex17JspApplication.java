38ae7122e376f5c4430be2dea8605ba9d8f4a295
yubin-im <ybyb130@gmail.com>
2024-04-08 11:02:30 +0900

    Spring: ex17 JSP 프로젝트 생성

---
+++ springboot/Ex17JSP/src/main/java/com/study/ex17jsp/Ex17JspApplication.java
@@ -0,0 +1,13 @@
+package com.study.ex17jsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex17JspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex17JspApplication.class, args);
+    }
+
+}
---
+++ springboot/Ex17JSP/src/test/java/com/study/ex17jsp/Ex17JspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.ex17jsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex17JspApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
