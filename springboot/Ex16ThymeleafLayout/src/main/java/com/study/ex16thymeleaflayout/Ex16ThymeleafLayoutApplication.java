e97e06ae7999e83f1bc703b1a2ad506b8c916a6b
yubin-im <ybyb130@gmail.com>
2024-04-08 09:30:01 +0900

    Spring: ex16 Thymeleaf Layout- 프로젝트 생성

---
+++ springboot/Ex16ThymeleafLayout/src/main/java/com/study/ex16thymeleaflayout/Ex16ThymeleafLayoutApplication.java
@@ -0,0 +1,13 @@
+package com.study.ex16thymeleaflayout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex16ThymeleafLayoutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex16ThymeleafLayoutApplication.class, args);
+    }
+
+}
