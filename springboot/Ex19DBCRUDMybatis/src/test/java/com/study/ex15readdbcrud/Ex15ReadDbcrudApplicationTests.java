db22624838c9711e4ce32e62b2851ffbbf431abb
yubin-im <ybyb130@gmail.com>
2024-04-09 13:30:26 +0900

    Spring: ex19 MyBatis- 프로젝트 생성(템플릿 복제)

---
+++ springboot/Ex19DBCRUDMybatis/src/test/java/com/study/ex15readdbcrud/Ex15ReadDbcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.ex15readdbcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex15ReadDbcrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
