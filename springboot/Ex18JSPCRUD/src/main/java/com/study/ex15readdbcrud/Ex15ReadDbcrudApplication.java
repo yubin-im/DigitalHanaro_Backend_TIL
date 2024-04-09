ea0d1a051982ac9b047c9599dc8183a3db24fe2c
yubin-im <ybyb130@gmail.com>
2024-04-09 09:21:33 +0900

    Spring: ex18 JSP CRUD- 프로젝트 생성

---
+++ springboot/Ex18JSPCRUD/src/main/java/com/study/ex15readdbcrud/Ex15ReadDbcrudApplication.java
@@ -0,0 +1,13 @@
+package com.study.ex15readdbcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex15ReadDbcrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex15ReadDbcrudApplication.class, args);
+    }
+
+}
