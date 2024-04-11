ccd5da86cc5b8d629b8a7eab71ade374de6eef5d
yubin-im <ybyb130@gmail.com>
2024-04-11 09:23:50 +0900

    스프링 연습문제: 11 자판기 with DB- 프로젝트 생성

---
+++ springbootExercise/Pr11VMDB/src/main/java/com/example/pr11vmdb/Pr11VmdbApplication.java
@@ -0,0 +1,13 @@
+package com.example.pr11vmdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pr11VmdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pr11VmdbApplication.class, args);
+    }
+
+}
---
+++ springbootExercise/Pr11VMDB/src/test/java/com/example/pr11vmdb/Pr11VmdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pr11vmdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pr11VmdbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
