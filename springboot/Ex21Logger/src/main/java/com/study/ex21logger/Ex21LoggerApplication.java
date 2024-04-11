25c9bb2713ccb3a200a9eed0e822cfc764a0d664
yubin-im <ybyb130@gmail.com>
2024-04-11 17:14:10 +0900

    Spring: ex21 Logger- 프로젝트 생성

---
+++ springboot/Ex21Logger/src/main/java/com/study/ex21logger/Ex21LoggerApplication.java
@@ -0,0 +1,13 @@
+package com.study.ex21logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex21LoggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex21LoggerApplication.class, args);
+    }
+
+}
---
+++ springboot/Ex21Logger/src/test/java/com/study/ex21logger/Ex21LoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.ex21logger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex21LoggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
