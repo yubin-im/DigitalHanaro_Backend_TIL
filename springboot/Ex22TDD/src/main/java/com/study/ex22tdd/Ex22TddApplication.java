894862905e8545a147b11e0260ca0c694c96be23
yubin-im <ybyb130@gmail.com>
2024-04-12 09:20:32 +0900

    Spring: ex22 TDD- 프로젝트 생성

---
+++ springboot/Ex22TDD/src/main/java/com/study/ex22tdd/Ex22TddApplication.java
@@ -0,0 +1,13 @@
+package com.study.ex22tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex22TddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex22TddApplication.class, args);
+    }
+
+}
---
+++ springboot/Ex22TDD/src/test/java/com/study/ex22tdd/Ex22TddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.ex22tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex22TddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
