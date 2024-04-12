aaa6f35c4ee1f2c831d6b511a836985a4f68a793
yubin-im <ybyb130@gmail.com>
2024-04-12 14:33:14 +0900

    Spring: ex23 Scheduler- 프로젝트 생성

---
+++ springboot/Ex23Scheduler/src/main/java/com/study/ex23scheduler/Ex23SchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.study.ex23scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex23SchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex23SchedulerApplication.class, args);
+    }
+
+}
---
+++ springboot/Ex23Scheduler/src/test/java/com/study/ex23scheduler/Ex23SchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.ex23scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex23SchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
