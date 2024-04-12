7ef1fbe0d47aa15e793399a6174c301ad965c630
yubin-im <ybyb130@gmail.com>
2024-04-12 16:13:29 +0900

    스프링 연습문제: 12 자판기 with API&DB- 프로젝트 생성

---
+++ springbootExercise/Pr12VMAPIDB/src/main/java/com/study/pr12vmapidb/Pr12VmapidbApplication.java
@@ -0,0 +1,13 @@
+package com.study.pr12vmapidb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pr12VmapidbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pr12VmapidbApplication.class, args);
+    }
+
+}
---
+++ springbootExercise/Pr12VMAPIDB/src/test/java/com/study/pr12vmapidb/Pr12VmapidbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.pr12vmapidb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pr12VmapidbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
