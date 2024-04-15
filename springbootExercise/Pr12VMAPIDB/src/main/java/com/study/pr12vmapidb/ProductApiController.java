e49974de0472ae5ae84de7c03baf56d5350e210c
yubin-im <ybyb130@gmail.com>
2024-04-15 10:27:41 +0900

    스프링 연습문제 12: 자판기 with API & DB- 추가/수정/삭제 기능 REST API로 변환

--- springbootExercise/Pr12VMAPIDB/src/main/java/com/study/pr12vmapidb/ProductApiController.java
+++ springbootExercise/Pr12VMAPIDB/src/main/java/com/study/pr12vmapidb/ProductApiController.java
@@ -20,19 +20,20 @@ public class ProductApiController {
         return productList;
     }
 
-//    // 상품 추가
-//    @PostMapping("/add")
-//    public List<Product> add(@RequestBody AddReqDTO addReqDTO) {
-//        Product product = Product.builder()
-//                .name(addReqDTO.getName())
-//                .price(addReqDTO.getPrice())
-//                .limitDate(addReqDTO.getLimitDate())
-//                .build();
-//        repo.productList.add(product);
-//
-//        System.out.println("상품리스트: " + repo.productList);
-//        return repo.productList;
-//    }
+    // 상품 추가
+    @PostMapping("/add-action")
+    @ResponseBody
+    public List<Product> add(@RequestBody AddReqDTO addReqDTO) {
+        Product product = Product.builder()
+                .name(addReqDTO.getName())
+                .price(addReqDTO.getPrice())
+                .limitDate(addReqDTO.getLimitDate())
+                .build();
+        productRepository.save(product);
+
+        List<Product> productList = productRepository.findAll();
+        return productList;
+    }
 
     // 수정할 상품 불러오기
     @GetMapping("/edit")
@@ -43,33 +44,27 @@ public class ProductApiController {
         return "editProductForm";
     }
 
-//    // 상품 수정
-//    @PostMapping("/edit-action")
-//    public List<Product> updateProduct(@RequestBody ProductDTO productDTO) {
-//        Product product = repo.productList.get(productDTO.getIndex());
-//
-//        product.updateProduct(productDTO.getName(), productDTO.getPrice(), productDTO.getLimitDate());
-//
-//        repo.productList.set(productDTO.getIndex(), product);
-//
-//        System.out.println("수정된 상품: " + repo.productList);
-//        return repo.productList;
-//    }
-//
-//    // 상품 삭제
-//    @DeleteMapping("/delete")
-//    public List<Product> delete(@RequestBody Map<String, String> nameMap) {
-//        String name = nameMap.get("name");
-//
-//        Iterator<Product> iterator = repo.productList.iterator();
-//        while (iterator.hasNext()) {
-//            Product product = iterator.next();
-//            if (product.getName().equals(name)) {
-//                iterator.remove();
-//                break;
-//            }
-//        }
-//
-//        return repo.productList;
-//    }
+    // 상품 수정
+    @PostMapping("/edit-action")
+    @ResponseBody
+    public List<Product> updateProduct(@RequestBody ProductDTO productDTO) {
+        Product product = productRepository.findById(productDTO.getId()).orElse(null);
+        product.updateProduct(productDTO.getName(), productDTO.getPrice(),productDTO.getLimitDate());
+        productRepository.save(product);
+
+        List<Product> productList = productRepository.findAll();
+        return productList;
+    }
+
+    // 상품 삭제
+    @PostMapping("/delete")
+    @ResponseBody
+    public List<Product> delete(@RequestBody Map<String, Long> idMap) {
+        Long id = idMap.get("id");
+        Product product = productRepository.findById(id).orElse(null);
+        productRepository.delete(product);
+
+        List<Product> productList = productRepository.findAll();
+        return productList;
+    }
 }
