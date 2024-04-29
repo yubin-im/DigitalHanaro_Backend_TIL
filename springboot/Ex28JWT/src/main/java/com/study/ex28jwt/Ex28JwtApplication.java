e8a08f69c322c642239a6d1c1e1438efe6ec5433
yubin-im <ybyb130@gmail.com>
2024-04-29 10:17:27 +0900

    Spring: ex28 JWT- 프로젝트 생성

---
+++ springboot/Ex28JWT/src/main/java/com/study/ex28jwt/Ex28JwtApplication.java
@@ -0,0 +1,13 @@
+package com.study.ex28jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex28JwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex28JwtApplication.class, args);
+    }
+
+}
---
+++ springboot/Ex28JWT/src/test/java/com/study/ex28jwt/Ex28JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.ex28jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex28JwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
