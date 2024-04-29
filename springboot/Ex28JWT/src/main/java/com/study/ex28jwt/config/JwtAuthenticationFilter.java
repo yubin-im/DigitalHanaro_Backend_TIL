e52b1b53f80ad2a56bee1f254b2315410c966c7f
yubin-im <ybyb130@gmail.com>
2024-04-29 16:01:07 +0900

    Spring: ex28 JWT- JWT 실습 추가

--- springboot/Ex28JWT/src/main/java/com/study/ex28jwt/config/JwtAuthenticationFilter.java
+++ springboot/Ex28JWT/src/main/java/com/study/ex28jwt/config/JwtAuthenticationFilter.java
@@ -17,16 +17,17 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
     private final JwtUtil jwtTokenProvider;
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        // 헤더에서 JWT를 받아온다.
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        //헤더에서 JWT를 받아온다.
         String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
-
-        // 유효한 토큰인지 확인한다.
-        if(token != null && jwtTokenProvider.validateToken(token)) {
-            // 토큰이 유효하면 토큰으로부터 유저 정보를 가져온다.
-            Authentication authentication = jwtTokenProvider.getAuthentication(token);
-            // SecurityContext에 Authentication 객체를 저장해준다.
+        //유효한 토큰인지 확인한다.
+        if(token != null && jwtTokenProvider.validateToken(token)){
+            //토큰이 유효하면 토큰으로부터 유저 정보를 가져온다.
+            Authentication authentication =
+                    jwtTokenProvider.getAuthentication(token);
+            //SecurityContext에 Authentication객체를 저장해 준다.
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
+        filterChain.doFilter(request, response);
     }
 }
