4fd3fa492cecaf2612251be85e21454c20505d10
yubin-im <ybyb130@gmail.com>
2024-05-02 09:26:37 +0900

    Spring: ex29 Reflection- 프로젝트 생성

---
+++ springboot/Ex29Reflection/src/main/java/com/study/springboot/Ex29ReflectionApplication.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex29ReflectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex29ReflectionApplication.class, args);
+    }
+
+}
